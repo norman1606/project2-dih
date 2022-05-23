d36fab45352f1669e3f30cd52124c65d51b2ddfb
norman1606 <augusto.norman@gmail.com>
2022-05-23 18:09:29 -0300

    incrementando router

--- projeto-estoque/src/views/MyLogin.vue
+++ projeto-estoque/src/views/MyLogin.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        <h1>Home!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-7 left-login">col-8</div>
+            <div class="col-5">col-4</div>
+        </div>       
     </div>
 </template>
+
+<style>
+html {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+}
+
+.row {
+    margin-left: 0;
+}
+
+.left-login {
+    background-color: #f2f2f2;
+}
+
+</style>
