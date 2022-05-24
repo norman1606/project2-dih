87377d7de9d87617a76ab66d71a1f5f0d988788f
norman1606 <augusto.norman@gmail.com>
2022-05-24 16:29:36 -0300

    iniciando pagina inventario

---
+++ projeto-estoque/src/views/MyInventary.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        <div class="sidebar">
+            
+        </div>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
